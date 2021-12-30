4e78e917dffcc68bc5af52403d3b7b0f338a7612
Sam Pink <samuel.pink@outlook.com>
2021-12-30 17:32:23 +0000

    updates to asset retrieval for rarity calc

--- opensea_assets.py
+++ opensea_assets.py
@@ -3,10 +3,14 @@ from pandas import json_normalize
 import requests
 import json
 import pandas as pd
+from math import ceil
+import numpy as np
+from database import *
+import re
 
 
 def get_opensea_asset(
-    offset=0, collection="boredapeyachtclub", api_key="3eb775e344f14798b49718e86f55608c"
+    offset, collection, limit=50, api_key="3eb775e344f14798b49718e86f55608c"
 ):
 
     url = "https://api.opensea.io/api/v1/assets"
@@ -14,7 +18,7 @@ def get_opensea_asset(
     params = {
         "collection_slug": collection,
         "offset": offset * 50,
-        "limit": 50,
+        "limit": limit,
     }
 
     headers = {"Accept": "application/json", "X-API-KEY": api_key}
@@ -35,51 +39,102 @@ def get_opensea_metadata(collection, api_key="3eb775e344f14798b49718e86f55608c")
     return response
 
 
-get_opensea_metadata("boredapeyachtclub")
+x = response = get_opensea_asset(offset=0, collection="ape-gang", limit=2)
 
-assets = pd.DataFrame()
-assets_traits = pd.DataFrame()
-for i in range(0, 2):  # change this to get more assets
-    response = get_opensea_asset(i)
-    for asset in response["assets"]:
-        df = pd.json_normalize(asset)
-        traits = df.traits.item()
-        tr = {}
-        tr["name"] = df.name.item()
-        for trait in traits:
-            tr[trait["trait_type"]] = trait["value"]
-        assets_traits = assets_traits.append(pd.DataFrame(tr, index=[0]))
-        assets = assets.append(df)
 
-# count number of traits of each apr
-trait_list = assets_traits.columns
+def get_collection_assets(collection, id_col="token_id"):
 
-assets_traits["trait_n"] = assets_traits[trait_list].count(axis=1)
-
-# calculate rarity (proportion of apes with each single trait)
-apes = assets_traits
-apes_with_rarity = apes
-for i in trait_list:
-    apes_with_trait_i = apes[apes[i].notna()].shape[0]
-    i_counts = apes[i].value_counts()
-    i_proportion = i_counts / apes_with_trait_i
-    apes_with_rarity = apes_with_rarity.merge(
-        i_proportion, left_on=i, right_index=True, suffixes=("", "_rarity"), how="left"
+    # work out how many loops to perform
+    metadata = get_opensea_metadata(collection=collection)
+    n_items = int(metadata["collection"]["stats"]["count"])
+    n_api_calls = ceil(n_items / 50)  # calculate number of api calls we'll need to make
+    print(
+        f"{collection} has {n_items} NFTs, {n_api_calls} api calls will be made to retrieve the asset data."
     )
 
-# create an array of the columns with the rarity
-rarity_list = []
-for i in trait_list:
-    rarity_list.append(i + "_rarity")
+    # work out which traits the collection has
+    traits = list(metadata["collection"]["traits"].keys())
+
+    assets = pd.DataFrame()
+    for i in range(0, n_api_calls):  # change this to get more assets
+        response = get_opensea_asset(offset=i, collection=collection)
+        for asset in response["assets"]:
+            df = pd.json_normalize(asset)
+            for t in asset["traits"]:
+                df[t["trait_type"]] = t["value"]
+            assets = assets.append(df)
+        print(
+            f"{i} of {n_api_calls} API calls have been made, {assets.shape[0]} {collection}'s retrieved!"
+        )
+
+    assets = assets.reset_index()
+
+    # count number of traits of each apr
+    assets_traits = assets[[id_col] + traits]
+
+    # is id contained in string with # followed by digits
+    first_id = assets_traits[id_col][0]
+    if re.search("#\d+", first_id) is not None and isinstance(first_id, str):
+        assets_traits = assets_traits.assign(
+            asset_id=lambda x: x[id_col].str.extract("(\d+)")
+        )
+
+    assets_traits["trait_n"] = assets_traits[traits].count(axis=1)
+
+    traits.append("trait_n")
+
+    # calculate rarity (proportion of apes with each single trait)
+    rarity = assets_traits.copy()
+
+    for i in traits:
+        i_proportion = assets_traits[i].value_counts(dropna=False, normalize=True)
+        rarity = rarity.merge(
+            i_proportion,
+            left_on=i,
+            right_index=True,
+            suffixes=("", "_rarity"),
+            how="left",
+        )
+
+    # create an array of the columns with the rarity
+    rarity_list = [t + "_rarity" for t in traits]
+
+    factorial_rarity = 1 / rarity[rarity_list]
+    rarity["factoral_rarity"] = factorial_rarity.sum(axis=1)
+    rarity = rarity.sort_values("factoral_rarity", ascending=False)
+    rarity["rarity_rank"] = np.arange(1, rarity.shape[0] + 1, 1)
+
+    write_mongo(
+        collection=f"{collection}_traits",
+        data=rarity,
+        overwrite=True,
+        database_name="mvh",
+    )
+
+    write_mongo(
+        collection=f"{collection}_asset-all-info",
+        data=assets,
+        overwrite=True,
+        database_name="mvh",
+    )
 
 
-# calculate mean of all singe trait rarity
-apes_with_rarity["mean_trait_rarity"] = apes_with_rarity[rarity_list].mean(axis=1)
-
-# calculate rarity of most rare trait
-apes_with_rarity["min_trait_rarity"] = apes_with_rarity[rarity_list].min(axis=1)
-
-# calculate rarity of least rare trait
-apes_with_rarity["max_trait_rarity"] = apes_with_rarity[rarity_list].max(axis=1)
-
-print(apes_with_rarity)
+get_collection_assets("ape-gang-old", id_col="name")
+collections = [
+    "bored-ape-kennel-club",
+    # "boredapeyachtclub",
+    "chromie-squiggle-by-snowfro",
+    "cool-cats-nft",
+    "cryptoadz-by-gremplin",
+    "cryptomories",
+    "cyberkongz",
+    "guttercatgang",
+    "gutterdogs",
+    "lazy-lions",
+    "mutant-ape-yacht-club",
+    "pudgypenguins",
+    "the-doge-pound",
+    "toucan-gang",
+]
+for x in collections:
+    get_collection_assets(x, id_col="name")
